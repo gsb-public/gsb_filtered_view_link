577c99757ff4ed2a3e19f446fcd66badeb813769
Adam Moore <redndahead@gmail.com>
2015-08-12 10:26:04 -0700

    Initial commit.

---
+++ js/gsb_filtered_view_link.js
@@ -0,0 +1,25 @@
+(function ($) {
+  Drupal.behaviors.gsb_filtered_view_link = {
+    attach: function (context, settings) {
+      $link = $('<a href="#">' + Drupal.t('Show the direct url to this filter') + '</a>');
+      $('.views-exposed-form').prepend($link);
+      $link.click(function(e) {
+        e.preventDefault();
+        url = location.protocol + "//" + location.hostname + location.pathname + "?";
+        $('.view-filters input[type="checkbox"]:checked').each(function() {
+          name = jQuery(this).attr('name');
+          tid = jQuery(this).attr('value');
+          url += name + '=' + tid + '&';
+        });
+        $('.view-filters input[type="text"]').each(function() {
+          if ($(this).val()) {
+            url += $(this).attr('name') + "=" + $(this).val() + '&';
+          }
+        });
+        url = url.substring(0, url.length - 1);
+
+        alert(Drupal.t("You can copy this link") + ": \n\n" + url);
+      });
+    }
+  }
+})(jQuery);
