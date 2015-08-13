183a408e3f2e43325a602fc2c835d06f8f1c4353
Adam Moore <redndahead@gmail.com>
2015-08-13 15:31:26 -0700

    WP-5859: Refactor code. Add a text field to be able to copy and paste data from.

--- js/gsb_filtered_view_link.js
+++ js/gsb_filtered_view_link.js
@@ -1,27 +1,74 @@
 (function ($) {
   Drupal.behaviors.gsb_filtered_view_link = {
     attach: function (context, settings) {
+
+
+      if (typeof Drupal.settings.gsb_filtered_view_link == 'undefined') {
+        Drupal.settings.gsb_filtered_view_link = {};
+      }
+
+      if (typeof Drupal.settings.gsb_filtered_view_link.open == 'undefined') {
+        Drupal.settings.gsb_filtered_view_link.open == false;
+      }
+
+      if (Drupal.settings.gsb_filtered_view_link.open) {
+        link_text = Drupal.t('Hide the direct url field');
+      }
+      else {
+        link_text = Drupal.t('Show the direct url to this filter');
+      }
+
       $('.gsb-direct-link').remove();
-      $link = $('<a class="gsb-direct-link" href="#">' + Drupal.t('Show the direct url to this filter') + '</a>');
-      $('.views-exposed-form').prepend($link);
+      $wrapper = $('<div class=""gsb-direct-link"></div>');
+      $link = $('<a href="#">' + link_text + '</a>');
+      $wrapper.prepend($link);
+      $wrapper.append('<br />');
+      $input = $('<input class="ctools-auto-submit-exclude link-box" style="width: 550px; margin: 10px;" type="text" value="" />');
+      $wrapper.append($input);
+      $('.views-exposed-form').prepend($wrapper);
+
+      if (!Drupal.settings.gsb_filtered_view_link.open) {
+        $input.hide();
+      }
+
+      Drupal.gsb_filtered_view_link.buildURL($input);
 
       $link.click(function(e) {
         e.preventDefault();
-        url = location.protocol + "//" + location.hostname + location.pathname + "?";
-        $('.view-filters input[type="checkbox"]:checked').each(function() {
-          name = jQuery(this).attr('name');
-          tid = jQuery(this).attr('value');
-          url += name + '=' + tid + '&';
-        });
-        $('.view-filters input[type="text"]').each(function() {
-          if ($(this).val()) {
-            url += $(this).attr('name') + "=" + $(this).val() + '&';
-          }
-        });
-        url = url.substring(0, url.length - 1);
+        if (Drupal.settings.gsb_filtered_view_link.open) {
+          $input.hide();
+          $link.text(Drupal.t('Show the direct url to this filter'));
+          Drupal.settings.gsb_filtered_view_link.open = false;
+        }
+        else {
+          $input.show();
+          $link.text(Drupal.t('Hide the direct url field'));
+          Drupal.settings.gsb_filtered_view_link.open = true;
+        }
+      });
 
-        alert(Drupal.t("You can copy this link") + ": \n\n" + url);
+      $('.view-filters input[type="checkbox"]:checked, .view-filters input[type="text"]:not(".link-box")').change(function() {
+        Drupal.gsb_filtered_view_link.buildURL($input);
       });
     }
+  }
+
+  Drupal.gsb_filtered_view_link = Drupal.gsb_filtered_view_link || {};
+
+  Drupal.gsb_filtered_view_link.buildURL = function($input) {
+    url = location.protocol + "//" + location.hostname + location.pathname + "?";
+    $('.view-filters input[type="checkbox"]:checked').each(function() {
+      name = $(this).attr('name');
+      tid = $(this).attr('value');
+      url += name + '=' + tid + '&';
+    });
+    $('.view-filters input[type="text"]:not(".link-box")').each(function() {
+      if ($(this).val()) {
+        url += $(this).attr('name') + "=" + $(this).val() + '&';
+      }
+    });
+    url = url.substring(0, url.length - 1);
+
+    $input.val(url);
   }
 })(jQuery);
